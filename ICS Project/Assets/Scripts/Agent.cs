44c38d53f3eefdaf862a8d33761719c71335c588
Kasia <ka.fojcik@gmail.com>
2020-05-17 12:40:54 +0200

    AgentPrefab

---
+++ ICS Project/Assets/Scripts/Agent.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Seirs
+{
+    [RequireComponent(typeof(Renderer))] // require from object to have Renderer script
+    public class Agent : MonoBehaviour
+    {
+        private Renderer rend;
+
+        [SerializeField] private Material redMaterial;
+        [SerializeField] private Material blueMaterial;
+        [SerializeField] private Material greenMaterial;
+        [SerializeField] private Material yellowMaterial;
+
+        public int Id { get; set; }
+        public Material RedMaterial { get => redMaterial; set => redMaterial = value; }
+        public Material BlueMaterial { get => blueMaterial; set => blueMaterial = value; }
+        public Material GreenMaterial { get => greenMaterial; set => greenMaterial = value; }
+        public Material YellowMaterial { get => yellowMaterial; set => yellowMaterial = value; }
+
+        public void SetPosition(float x, float y, float z = 0)
+        {
+            transform.position = new Vector3(x, y, z);
+        }
+
+        public void SetRadius(float r)
+        {
+            transform.localScale = Vector3.one * r;
+        }
+
+        public void SetColor(State s)
+        {
+            rend = rend ?? GetComponent<Renderer>();
+
+            switch (s)
+            {
+                case State.S:
+                    Debug.Log("green");
+                    rend.material = greenMaterial;
+                    break;
+                case State.E:
+                    rend.material = yellowMaterial;
+                    break;
+                case State.I:
+                    rend.material = redMaterial;
+                    break;
+                case State.R:
+                    rend.material = blueMaterial;
+                    break;
+            }
+        }
+    }
+
+    public enum State
+    {
+        S,
+        E,
+        I,
+        R
+    }
+}
---
+++ ICS Project/Assets/Scripts/AgentManager.cs
@@ -0,0 +1,40 @@
+﻿using Seirs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Seirs
+{
+    public class AgentManager : MonoBehaviour
+    {
+        [SerializeField] private Agent agentPrefab;
+        public Agent AgentPrefab { get => agentPrefab; set => agentPrefab = value; }
+        public Dictionary<int, Agent> Agents { get; set; }
+
+        public void SetAgent(int id, float x, float y, float r, State st)
+        {
+            Agent agent;
+            if(!Agents.TryGetValue(id, out agent))
+            {
+                agent = Instantiate(agentPrefab);    
+                Agents.Add(id, agent);   
+                agent.Id = id;
+            }
+            agent.SetPosition(x, y);
+            agent.SetRadius(r);
+            agent.SetColor(st);
+        }
+
+        private void Awake()
+        {
+            Agents = new Dictionary<int, Agent>();
+        }
+
+        private void Start()
+        {
+            SetAgent(1, 0, 0, 1, State.S);
+            SetAgent(2, 1, 1, 1.5f, State.E);
+            SetAgent(3,-2, -2, 1, State.I);
+        }
+    }
+}
