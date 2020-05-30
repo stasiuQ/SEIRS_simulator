b3ee2dd125d65a736ea3beda0545147574f05b52
Your Name <wmacioszczyk@gmail.com>
2020-05-30 13:43:39 +0200

    [gitignore] add rules for gitignore

---
+++ ICS Project/Assets/Scripts/Models/BatchStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Seirs.Models
+{
+    public class BatchStats
+    
+    {
+        public int Size { get; set; } = 0;
+        public Queue<int[]> Stats { get; set; } = new Queue<int[]>();
+
+        public void Add(int[] stats)
+        {
+            Stats.Enqueue(stats);
+            Size++;
+        }
+
+        public bool CanAdd()
+        {
+            return Size < 100;
+        }
+
+        public IEnumerable<int[]> ToEnumerable() => Stats;
+    }
+}
